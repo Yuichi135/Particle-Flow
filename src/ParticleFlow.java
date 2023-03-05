5a945cb9b18d0bb7df64b23460470f56a2a3b1e4
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-03-05 12:34:16 +0100

    Setup project

---
+++ src/ParticleFlow.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ParticleFlow extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Particle Flow");
+        stage.show();
+    }
+}
